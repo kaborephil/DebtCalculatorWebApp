6e079f4c2f55df14da81c8cd7db28c4d2b752fb4
Pingdwende Philippe Kabore <kaborephil@gmail.com>
2019-05-07 23:16:27 -0400

    Add POCO class for AutoLoan

---
+++ DebtCalculatorWebApp/Models/AutoLoan.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DebtCalculatorWebApp.Models
+{
+    public class AutoLoan
+    {
+        public double AutoLoanAmount { get; set; }
+        public int AutoLoanTime { get; set; }
+        public double AutoLoanApr { get; set; }
+    }
+}
