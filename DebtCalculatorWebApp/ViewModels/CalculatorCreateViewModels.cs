06a5954842ec2df16b52e655a2b5e2a320d584fe
Pingdwende Philippe Kabore <kaborephil@gmail.com>
2019-05-08 07:37:52 -0400

    Add CalculatorCreateViewModels.cs in ViewModels folder

---
+++ DebtCalculatorWebApp/ViewModels/CalculatorCreateViewModels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DebtCalculatorWebApp.Models;
+
+namespace DebtCalculatorWebApp.ViewModels
+{
+    public class CalculatorCreateViewModels
+    {
+        public AutoLoan AutoLoan { get; set; }
+        public CreditCardLoan CreditCardLoan { get; set; }
+        public Mortgage Mortgage { get; set; }
+    }
+}
