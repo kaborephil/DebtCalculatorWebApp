97fdcf761ee17aa103b52718f8db60f0213a109c
Pingdwende Philippe Kabore <kaborephil@gmail.com>
2019-05-07 23:21:36 -0400

    Add POCO class for CreditCardLoan

---
+++ DebtCalculatorWebApp/Models/CreditCardLoan.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DebtCalculatorWebApp.Models
+{
+    public class CreditCardLoan
+    {
+        public double CreditCardLoanAmount { get; set; }
+        public int CreditCardLoanTime { get; set; }
+        public double CreditcardLoanApr { get; set; }
+    }
+}
