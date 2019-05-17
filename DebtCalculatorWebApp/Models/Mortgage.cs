f6f3cb12e4f54478a429ebb8e2b01a053879f93d
Pingdwende Philippe Kabore <kaborephil@gmail.com>
2019-05-16 21:18:53 -0400

    Add properties in Mortgage class

--- DebtCalculatorWebApp/Models/Mortgage.cs
+++ DebtCalculatorWebApp/Models/Mortgage.cs
@@ -7,8 +7,10 @@ namespace DebtCalculatorWebApp.Models
 {
     public class Mortgage
     {
+        public int MortgageId { get; set; }
         public double MortgageAmount { get; set; }
         public int MortgageTime { get; set; }
         public double MortgageApr { get; set; }
+        public DateTime RecordDate { get; set; }
     }
 }
