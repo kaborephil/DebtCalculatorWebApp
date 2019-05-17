7ed8e190156972bb650ac1ffe07f32fcdf7aaf0c
Pingdwende Philippe Kabore <kaborephil@gmail.com>
2019-05-16 21:15:31 -0400

    Add properties in CreditCardLoan class

--- DebtCalculatorWebApp/Models/CreditCardLoan.cs
+++ DebtCalculatorWebApp/Models/CreditCardLoan.cs
@@ -7,8 +7,10 @@ namespace DebtCalculatorWebApp.Models
 {
     public class CreditCardLoan
     {
+        public int CreditCardLoanId { get; set; }
         public double CreditCardLoanAmount { get; set; }
         public int CreditCardLoanTime { get; set; }
         public double CreditcardLoanApr { get; set; }
+        public DateTime RecordDate { get; set; }
     }
 }
